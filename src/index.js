6ad68c87aa779c3fa6c3ba6ecb40a45b083fa3d5
Слесарев Александр <aslesarev@htc-cs.ru>
2020-05-12 11:30:29 +0400

    добавил redux

--- src/index.js
+++ src/index.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import App from './App/App';
 import * as serviceWorker from './serviceWorker';
 
 import 'normalize.css';
+import store from './store/store';
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
     document.getElementById('root')
 );
 
