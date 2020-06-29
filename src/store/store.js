a8ecb7cd773e901202cdcee26b78e409092b41c7
Alexander <kobzon3000bmx@mail.ru>
2020-06-29 19:16:10 +0400

    install thunk

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,9 @@
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import rootReducer from './reducers/rootReducer';
 
 const reduxTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(); 
 
-const store = createStore(rootReducer, reduxTools);
+const store = createStore(rootReducer, compose(applyMiddleware(thunk),reduxTools));
 
 export default store;
