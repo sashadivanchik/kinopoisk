17f45c831096f8ca15c65d306c83094d9590c67f
Alexander <kobzon3000bmx@mail.ru>
2020-07-08 23:56:39 +0400

    save viewed movies to localStorage

--- src/store/store.js
+++ src/store/store.js
@@ -1,9 +1,19 @@
 import { createStore, compose, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
 import rootReducer from './reducers/rootReducer';
+import {saveState, loadState} from './localStorage';
 
 const reduxTools = window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(); 
 
-const store = createStore(rootReducer, compose(applyMiddleware(thunk), reduxTools));
+const persistedState = loadState('viewedMovies');
+
+const store = createStore(rootReducer, persistedState, compose(applyMiddleware(thunk), reduxTools));
+
+store.subscribe(() => {
+    const state = store.getState();
+    saveState({
+        viewedMovies: state.viewedMovies
+    }, 'viewedMovies');
+})
 
 export default store;
